6736b67fca514daa0ed4d1644cec0542fb31c41c
Steffen Schuhmann <mail@sschuhmann.de>
2015-06-25 11:28:20 +0200

    Database

--- models/database.js
+++ models/database.js
@@ -15,40 +15,91 @@ client.on('error', function(error) {
 });
 
 var manager = {
+
+	errorHandler: function(error) {
+		console.log(error);
+	},
 	
 	/*
 	 * Create the database tables.
 	 */
 	initDatabase: function() {
-		var query = client.query("CREATE TABLE IF NOT EXISTS sensor(id SERIAL PRIMARY KEY, description VARCHAR(40) not null, unit VARCHAR(10));	CREATE TABLE IF NOT EXISTS mission(id SERIAL PRIMARY KEY, description VARCHAR(80), start_time TIMESTAMP, end_time TIMESTAMP);");
-		query.on ('error', function(error) {
-			console.log(error);
-		});
+		var query = client.query(
+			'CREATE TABLE IF NOT EXISTS sensor(' +
+				'id SERIAL PRIMARY KEY, '+
+				'producer INTEGER references producer(id), ' +
+				'name VARCHAR(20) not null, ' +
+				'description VARCHAR(40), ' +
+				'unit VARCHAR(10)' +
+			');' +
+			'CREATE TABLE IF NOT EXISTS mission(' +
+				'id SERIAL PRIMARY KEY, '+ 
+				'description VARCHAR(80), ' +
+				'start_time TIMESTAMP, ' +
+				'end_time TIMESTAMP'+
+			');' +
+			'CREATE TABLE IF NOT EXISTS value(' +
+				'timestamp TIMESTAMP, ' +
+				'sensor_id INTEGER references sensor(id), ' +
+				'value decimal' +
+			');' +
+			'CREATE TABLE IF NOT EXISTS producer (' +
+				'id SERIAL PRIMARY KEY,' +
+				'name VARCHAR(20),' +
+				'description VARCHAR(80)' +
+			');'
+		);
+		
+		query.on ('error', errorHandler(error));
 	},
 	
 	/*
 	 * Add a new mission to the database
 	 */
 	createMission: function(mission) {
-		var query = client.query('INSERT INTO mission VALUES ($1, $2, $3);', [mission.description, mission.start_time, mission.end_time]);
+		var query = client.query(
+			'INSERT INTO mission VALUES ($1, $2, $3);', 
+			[mission.description, mission.start_time, mission.end_time]
+		);
 		
-		query.on('err', function(error) {
-			console.log(error);
-		});
+		query.on('err', errorHandler(error));
+	},
+	
+	createValue: function(value) {
+		var query = client.query (
+			'INSERT INTO value ($0, $1, $2);',
+			[value.timestamp, value.sensor_id, value.value]
+		);
+		
+		query.on('err', errorHandler(error));
 	},
 	
 	/*
 	 * Add a new sensor to the database
 	 */
 	createSensor: function(sensor) {
+		var query = client.query(
+			'INSERT INTO sensor VALUES ($1, $2, $3, $4);',
+			[sensor.producer, sensor.sensorname, sensor.description, sensor.unit]
+		);
+		
+		query.on('err', errorHandler(error));
+	},
 	
+	createProducer: function(producer) {
+		var query = client.query (
+			'INSERT INTO producer VALUES ($1, $2);',
+			[producer.producer_name, producer.description]
+		);
+		
+		query.on('err', errorHandler(error));
 	},
 	
 	/*
 	 * Create a new user 
 	 */
 	createUser: function (user) {
-	
+		
 	},
 	
 	/*
@@ -57,6 +108,8 @@ var manager = {
 	getAllMission: function () {
 		var results = [];
 		var query = client.query('SELECT * FROM mission;');
+		
+		query.on('err', errorHandler(error));
 		
 		query.on('row', function(row) {
 			results.push(row);
@@ -81,9 +134,7 @@ var manager = {
 			results.push(row);
 		});
 		
-		query.on('end', function() {
-			return results;
-		});
+		query.on('end', errorHandler(error));
 	}
 };
 
